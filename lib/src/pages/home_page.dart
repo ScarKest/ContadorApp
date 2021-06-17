b70b45f853aa69d8602479684ba151ab12467c89
Kestler Barrios <luiseduardo3213@gmail.com>
2021-06-17 14:29:42 -0600

    Agregando Columnas y estilo de Textos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final _style = TextStyle(fontSize: 25);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Contador'),
       ),
       body: Center(
-        child: Text('Contenido del centro'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Numero de Taps',
+              style: _style,
+            ),
+            Text(
+              '0',
+              style: _style,
+            )
+          ],
+        ),
       ),
     );
   }
